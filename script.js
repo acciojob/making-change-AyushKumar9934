54b4bf55191b9b45f84279e9e90ac02630b5bd64
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-30 15:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const makeChange = (c) => {
-  // your name here
-};
+    let quarters = Math.floor(c / 25);
+    c %= 25;
+
+    let dimes = Math.floor(c / 10);
+    c %= 10;
+
+    let nickels = Math.floor(c / 5);
+    c %= 5;
+
+    let pennies = c;
+
+    return { "q": quarters, "d": dimes, "n": nickels, "p": pennies };
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
